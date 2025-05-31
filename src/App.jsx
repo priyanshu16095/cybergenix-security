e7c4960b041042807255f6d2bbaeb1ee1b882be1
Priyanshu Gupta <priyanshu16095@gmail.com>
2025-06-01 02:10:45 +0530

    Implemented Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Accordion from './components/Accordion'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='app'>
       <Accordion />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='comp h-full flex-s vertical'>
+            <div className="footer-top highl flex-v gap">
+                <p className="title">Let’s connect</p>
+                <p>Feel free to reach out for collaborations or just a friendly ping.</p>
+            </div>
+            <div className='border-top flex-v gap-md'>
+                <div className="flex-v gap-sm">
+                    <a href="#">Contact us</a>
+                    <a href="#">Pricing</a>
+                    <a href="#">Reviews</a>
+                    <a href="#">About</a>
+                </div>
+                <div className="flex-h gap">
+                    <p>LINKEDIN</p>
+                    <p>INSTAGRAM</p>
+                    <p>X.COM</p>
+                </div>
+                <p>Designed by <a href="" className='bold'>Priyanshu Gupta</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
